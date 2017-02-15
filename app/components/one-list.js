2d3921ffcae061747f53ddf130cafbb51bce5a47
Jakub Liput <jakub.liput@gmail.com>
2017-02-15 15:14:59 +0100

    Example of lists and accordion

--- app/components/one-list.js
+++ app/components/one-list.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li',
-  classNames: ['one-sidebar-li'],
-
-  indexed: true
+  tagName: 'ul',
+  classNames: ['one-list'],
 });
