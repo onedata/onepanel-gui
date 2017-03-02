ed849af5f7dbf81bad8f819e4beef1ddddebc870
Jakub Liput <jakub.liput@gmail.com>
2017-03-02 17:00:16 +0100

    Work snapshot

--- app/components/new-cluster-installation.js
+++ app/components/new-cluster-installation.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Onepanel from 'npm:onepanel';
 
 const {
   assert,
@@ -7,12 +8,33 @@ const {
   }
 } = Ember;
 
+const {
+  ProviderConfiguration
+} = Onepanel;
+
 let ObjectPromiseProxy = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
 
+function getClusterHostname(hostnames) {
+  return hostnames.objectAt(0);
+}
+
+function getDomain(hostname) {
+  return hostname.split('.').splice(1).join('.');
+}
+
+function getHostnamesOfType(hosts, type) {
+  return hosts.filter(h => h[type]).map(h => h.hostname);
+}
+
+function getTaskId(location) {
+  return location.match(/^.*\/(.*)$/)[1];
+}
+
 export default Ember.Component.extend({
-  clusterManager: service('clusterManager'),
+  onepanelServer: service(),
+  clusterManager: service(),
   
-  primaryHost: null,
+  primaryClusterManager: null,
   
   /**
    * @type {ObjectPromiseProxy<Ember.A<HostInfo>>}
@@ -28,7 +50,54 @@ export default Ember.Component.extend({
       })
     );
   },
+  
+  configureProviderStarted(data) {
+    // TODO use real location from response header
+    let taskId = getTaskId('/api/v3/onepanel/tasks/f-qlxZUv-Qy7fbZBv4l7ekHJ4xlKES3AXS2gMYYTYf4');
+    return new Promise((resolve, reject) => {
+      let gettingTasksStatus = onepanelApi.getTaskStatus();
+      gettingTasksStatus.then((data) => {
+        // TODO: get status: ok, error, running
+        // if running -> set next timeout that will invoke
+      });
+      
+      let configureStatusPolling = setTimeout(() => {
+        
+      });
+    });
     
+    // TODO get name for cluster from user
+    let id = 'x';
+    
+    // TODO get "location" header, parse from /api/v3/onepanel/tasks/f-qlxZUv-Qy7fbZBv4l7ekHJ4xlKES3AXS2gMYYTYf4
+    
+    let onepanelApi = this.get('onepanelServer.onepanelApi');
+    
+    
+    // TODO: status polling 1s, then resolve
+    
+    let creatingCluster = this.get('clusterManager').createCluster({
+      id,
+      label: 'New cluster'
+    });
+    
+    // TODO handle reject
+    
+
+  },
+  
+  configureProviderFinished() {
+    creatingCluster.then(cluster => {
+      this.sendAction('clusterCreated', cluster);
+      this.sendAction('nextStep');
+    });
+  },
+  
+  configureProviderStartFailed(error) {
+    console.error(`Configure provider failed (${error.response.statusCode}): ${error.response.text}`)
+    debugger;
+  },
+  
   actions: {
     checkboxChanged(checked, event) {
       let hosts = this.get('hosts.content');
@@ -44,31 +113,79 @@ export default Ember.Component.extend({
       
       // TODO debug
       console.debug(JSON.stringify(hosts));
-    },
+  },
     
-    primaryHostChanged(hostname) {
-      this.set('primaryHost', hostname);
+    primaryClusterManagerChanged(hostname) {
+      this.set('primaryClusterManager', hostname);
     },
     
     deploy() {
+      let {
+        hosts,
+        onepanelServer,
+        primaryClusterManager
+      } = this.getProperties(
+         'hosts',
+         'onepanelServer',
+         'primaryClusterManager'
+      );
       // TODO use API
-      // TODO block if not valid
       
-      // TODO get name for cluster from user
-      let id = 'x';
+      hosts = hosts.content;
       
+      // TODO use oneprovider or onezone api
+      let api = onepanelServer.get('oneproviderApi');
       
-      let creatingCluster = this.get('clusterManager').createCluster({
-        id,
-        label: 'New cluster'
+      let hostnames = hosts.map(h => h.hostname);
+      
+      // TODO get domain... lists:usort(CmHosts ++ WrHosts ++ DbHosts), get_domain(hd(Hosts))
+      let domainName = getDomain(getClusterHostname(hostnames));
+      let autoDeploy = true;
+      let clusterManagers = getHostnamesOfType(hosts, 'clusterManager');
+      
+      // FIXME
+      // primary cluster manager should be first on the list
+      // let primaryCMIndex = clusterManagers.indexOf(primaryClusterManager);
+      // [clusterManagers[0], clusterManagers[primaryCMIndex]] =
+      //   [primaryClusterManager, clusterManagers[0]];
+      
+      let clusterWorkers = getHostnamesOfType(hosts, 'clusterWorker');
+      let databases = getHostnamesOfType(hosts, 'database');
+      
+      let nodes = {};
+      hostnames.forEach(hn => {
+        nodes[hn] = {
+          hostname: hn
+        };
       });
       
-      // TODO handle reject
-      
-      creatingCluster.then(cluster => {
-        this.sendAction('clusterCreated', cluster);
-        this.sendAction('nextStep');
+      var providerConfiguration = ProviderConfiguration.constructFromObject({
+        cluster: {
+          domainName,
+          autoDeploy,
+          nodes,
+          managers: {
+            mainNode: primaryClusterManager,
+            nodes: clusterManagers,
+          },
+          workers: {
+            nodes: clusterWorkers,
+          },
+          databases: {
+            nodes: databases
+          } 
+        }
       });
-    }
+      
+      let configuringProvider = api.configureProvider(providerConfiguration);
+      
+      configuringProvider.then(
+        this.configureProviderStarted.bind(this),
+        this.configureProviderStartFailed.bind(this)
+      );
+      
+      // TODO do not allow if not invalid data
+
+    },
   }
 });
