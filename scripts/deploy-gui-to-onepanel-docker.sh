965f770594327ce3f35b7e0ab9073482e231cdd1
Jakub Liput <jakub.liput@gmail.com>
2017-03-22 10:35:19 +0100

    VFS-3081 More configuration in deploy script

--- scripts/deploy-gui-to-onepanel-docker.sh
+++ scripts/deploy-gui-to-onepanel-docker.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# TODO oneprovider/onezone switch
-SERVICE_NAME=onezone
+[ -z $ONEPANEL_SERVICE_TYPE ] && ONEPANEL_SERVICE_TYPE=onezone
 
-TMP_GUI_PARENT_DIR=./tmp
+[ -z $TMP_GUI_PARENT_DIR ] && TMP_GUI_PARENT_DIR=./tmp
 TMP_GUI_DIR=${TMP_GUI_PARENT_DIR}/gui_static
 
-DOCKER_ID=`docker ps | grep ${SERVICE_NAME} | head -1 | cut -f1 -d" "`
+DOCKER_ID=`docker ps | grep ${ONEPANEL_SERVICE_TYPE} | head -1 | cut -f1 -d" "`
 
-rm -rf $TMP_GUI_DIR &&
+DOCKER_GUI_PARENT_DIR=/root/bin/node/data
+DOCKER_GUI_DIR=${DOCKER_GUI_PARENT_DIR}/gui_static
+
+rm -r $TMP_GUI_DIR
+echo "Copying dist to ${TMP_GUI_DIR}..."
 cp -r dist $TMP_GUI_DIR &&
-docker exec ${DOCKER_ID} rm -rf /root/bin/node/data/gui_static &&
-docker cp $TMP_GUI_DIR ${DOCKER_ID}:/root/bin/node/data/ &&
+docker exec ${DOCKER_ID} rm -rf ${DOCKER_GUI_DIR} &&
+echo "Copying ${TMP_GUI_DIR} to ${DOCKER_GUI_DIR} in ${DOCKER_ID} container..."
+docker cp $TMP_GUI_DIR ${DOCKER_ID}:${DOCKER_GUI_PARENT_DIR} &&
 
 echo "Deploying to ${SERVICE_NAME} done!"
