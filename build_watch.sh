49665bc963625da19db8439dbbc83f683b8a01be
Jakub Liput <jakub.liput@gmail.com>
2017-03-24 15:01:20 +0100

    VFS-3123 Added build files

---
+++ build_watch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#####################################################################
+# @author Lukasz Opiola
+# @copyright (C): 2016 ACK CYFRONET AGH
+# This software is released under the MIT license
+# cited in 'LICENSE.txt'.
+#####################################################################
+# usage:
+# ./build_watch.sh
+#
+# This script starts an ember build process that watches for changes in
+# src dir and automatically rebuilds GUI release.
+#####################################################################
+
+SRC_DIR=src
+REL_DIR=rel
+
+# please set env variable ONEPANEL_SERVICE_TYPE to "zone" or "provider"
+# the default is "zone"
+
+cd ${SRC_DIR} && ember build --watch --output-path=../${REL_DIR}
