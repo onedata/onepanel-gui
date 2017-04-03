ea7c516afbcfde4b84860b5429ff3f97cd384e79
Jakub Liput <jakub.liput@gmail.com>
2017-04-03 14:17:07 +0200

    VFS-3084 Using Mocha/Chai test libraries

--- src/tests/test-helper.js
+++ src/tests/test-helper.js
@@ -1,8 +1,4 @@
 import resolver from './helpers/resolver';
-import './helpers/responsive';
-
-import {
-  setResolver
-} from 'ember-qunit';
+import { setResolver } from 'ember-mocha';
 
 setResolver(resolver);
