36413b9139d651c814773feb948148059a4a088a
Jakub Liput <jakub.liput@gmail.com>
2017-04-06 11:59:36 +0200

    VFS-3084 Better error message on promise load fail

--- src/app/components/promise-proxy-container.js
+++ src/app/components/promise-proxy-container.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
+import { invokeAction } from 'ember-invoke-action';
 
 const {
   Component,
 } = Ember;
 
 export default Component.extend({
+  tagName: '',
+
   /**
    * @type {ObjectPromiseProxy}
    */
   proxy: null,
+
+  actions: {
+    toggleShowDetails() {
+      this.toggleProperty('showDetails');
+    },
+  }
 });
