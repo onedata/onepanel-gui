ede8a116cb506f2481e17596c7685b49e133e1e7
Jakub Liput <jakub.liput@gmail.com>
2017-04-26 12:09:53 +0200

    VFS-3113 Added some JSDoc

--- src/app/components/zone-installation-form.js
+++ src/app/components/zone-installation-form.js
@@ -1,8 +1,18 @@
+/**
+ * Additional options for zone deployment
+ *
+ * @module components/zone-installation-form
+ * @author Jakub Liput
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import OneFormSimple from 'onepanel-gui/components/one-form-simple';
 import layout from 'onepanel-gui/templates/components/one-form-simple';
 import { validator, buildValidations } from 'ember-cp-validations';
 import { invokeAction } from 'ember-invoke-action';
 
+// TODO i18n
 const FORM_FIELDS = [{
   name: 'name',
   type: 'text',
