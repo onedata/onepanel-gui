0c79ea030a838e6d52d0f2eb3f64a4e02bd485db
Jakub Liput <jakub.liput@gmail.com>
2017-04-28 13:51:09 +0200

    VFS-3121 ember-plainable addon

---
+++ src/lib/ember-plainable/addon/utils/plain-copy.js
@@ -0,0 +1,28 @@
+// Original implementation: http://stackoverflow.com/a/15002755
+
+import Ember from 'ember';
+import Plainable from 'ember-plainable/mixins/plainable';
+
+/**
+ * Somehow hacky method to check if object is Ember Object
+ * @returns {boolean} true if obj is Ember.Object
+ */
+function isEmberObject(obj) {
+  return obj.reopen !== undefined;
+}
+
+export default function plainCopy(obj) {
+  if (Ember.isArray(obj)) {
+    return obj.map(plainCopy);
+  } else if (typeof (obj) === "object") {
+    if (Plainable.detect(obj)) {
+      return obj.plainCopy();
+    } else if (!isEmberObject(obj)) {
+      return obj;
+    } else {
+      throw new Error(Ember.String.fmt("%@ is not Plainable", [obj]));
+    }
+  } else {
+    return obj;
+  }
+}
