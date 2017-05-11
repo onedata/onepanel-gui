2ab475dbc96cd29ec7c10df426bf3f3509000646
Michał Borzęcki <michal.borzecki@hotmail.com>
2017-05-11 13:36:23 +0200

    VFS-3176 unit tests for one-form component

---
+++ src/tests/unit/components/one-form-test.js
@@ -0,0 +1,96 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import Ember from 'ember';
+
+
+describe('Unit | Component | one form', function () {
+  setupComponentTest('one-form', {
+    unit: true
+  });
+
+  beforeEach(function() {
+    const FIELDS = {
+      main: [Ember.Object.create({
+        name: 'first',
+        type: 'text',
+      })],
+      another: [Ember.Object.create({
+        name: 'second',
+        type: 'text',
+      })]
+    };
+    const allFields = Object.values(FIELDS).reduce((a, b) => a.concat(b));
+    const currentFields = FIELDS.main;
+    const allFieldsValues = Ember.Object.create({
+      main: Ember.Object.create({first: null}),
+      another: Ember.Object.create({second: null}),
+    });
+    const currentFieldsPrefix = 'main';
+    const validations = Ember.Object.create({
+      errors: [
+        Ember.Object.create({
+          attribute: 'allFieldsValues.main.first',
+          message: 'error!'
+        })
+      ]
+    });
+    this.subject().setProperties({
+      allFields,
+      currentFields,
+      allFieldsValues,
+      currentFieldsPrefix,
+      validations
+    });
+    this.subject().prepareFields();
+  });
+
+  it('it detects errors while validation', function () {
+    expect(this.subject().get('isValid'), 'form is invalid').to.eq(false);
+
+    // simulates text input to show an error message
+    this.subject().changeFormValue('first', 'sth');
+    expect(this.subject().get('currentFields')[0].get('message'), 'field has error (after edition)').to.eq('error!');
+  });
+
+  it('it ignores errors in another fields group', function () {
+    this.subject().set('currentFieldsPrefix', 'another');
+    expect(this.subject().get('isValid'), 'form is valid').to.eq(true);
+    expect(this.subject().get('currentFields')[0].get('message'), 'field has no error').to.be.empty;
+  });
+
+  it('it ignores errors if field was not edited', function () {
+    expect(this.subject().get('isValid'), 'form is invalid').to.eq(false);
+    expect(this.subject().get('currentFields')[0].get('message'), 'field has no error').to.be.empty;
+  });
+
+  it('it detects that new errors have occurred', function () {
+    const error = this.subject().get('validations').get('errors')[0];
+    this.subject().get('validations').set('errors', []);
+    expect(this.subject().get('isValid'), 'form is valid when errors disappear').to.eq(true);
+    this.subject().get('validations').set('errors', [error]);
+    expect(this.subject().get('isValid'), 'form is again invalid when errors occurred').to.eq(false);
+  });
+
+  it('it can reset fields state and value', function () {
+    this.subject().changeFormValue('first', 'sth');
+    this.subject().resetFormValues();
+    expect(this.subject().get('isValid'), 'form is still invalid after reset').to.eq(false);
+    expect(this.subject().get('fieldValues.first'), 'field is empty after reset').to.be.empty;
+    expect(this.subject().get('currentFields')[0].get('message'), 'field has no error after reset').to.be.empty;
+  });
+
+  it('it detects that given field is not in current fields group', function () {
+    expect(this.subject().isKnownField('first'), 
+      'form recognizes field, which exists in current fields group').to.eq(true);
+    expect(this.subject().isKnownField('second'), 
+      'form does not recognize field, which does not exists in current fields group').to.eq(false);
+  });
+
+  it('it allows to change field value', function () {
+    expect(this.subject().get('formValues.first'), 'initial field value == null').to.eq(null);
+    this.subject().changeFormValue('first', 'sth');
+    expect(this.subject().get('formValues.first'), 
+      'field value changed after changeFormValue method call').to.eq('sth');
+  });
+});
