8709cff3f552114ca1e6221d0c38779b4176e61e
Michał Borzęcki <michal.borzecki@hotmail.com>
2017-05-17 12:25:11 +0200

    VFS-3211 PR tasks

--- src/app/helpers/data-type.js
+++ src/app/helpers/data-type.js
@@ -1,3 +1,12 @@
+/**
+ * Returns type of given data (`typeof` operator alias).
+ *
+ * @module helpers/data-type
+ * @author Michal Borzecki
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import Ember from 'ember';
 
 export function dataType([ data ]/*, hash*/) {
