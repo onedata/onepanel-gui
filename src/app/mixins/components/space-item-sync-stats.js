2714a99ca5c4116cbc6e1c1c8a1d971e7379408e
Jakub Liput <jakub.liput@gmail.com>
2017-07-11 22:49:08 +0200

    VFS-3283 Restart stats fetching after period change even if it was paused

--- src/app/mixins/components/space-item-sync-stats.js
+++ src/app/mixins/components/space-item-sync-stats.js
@@ -322,6 +322,7 @@ export default Mixin.create({
           _prevSyncInterval: currentSyncInterval,
           timeStatsLoading: true,
           timeStatsError: null,
+          statsFrozen: false,
         });
       }
     },
