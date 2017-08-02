d4a8c3d8d11cd0448eff2e03ed36471fb2a7571f
Jakub Liput <jakub.liput@gmail.com>
2017-08-02 16:14:55 +0200

    VFS-3398 Application error route

---
+++ src/app/locales/en/components/application-error.js
@@ -0,0 +1,6 @@
+export default {
+  message: 'We are sorry, but a fatal error occured',
+  showDetails: 'Show details...',
+  hideDetails: 'Hide details',
+  noDetails: 'Unfortunately, no details are available',
+};
