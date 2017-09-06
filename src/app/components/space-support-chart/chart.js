d4636c60f61dfd387f8f80821fcb069c77a649f6
Michal Borzecki <michal.borzecki@hotmail.com>
2017-09-06 08:29:10 +0200

    VFS-3569 PR fixes

--- src/app/components/space-support-chart/chart.js
+++ src/app/components/space-support-chart/chart.js
@@ -8,10 +8,19 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
+import Ember from 'ember';
 import OnePieChart from 'onedata-gui-common/components/one-pie-chart';
 import bytesToString from 'onedata-gui-common/utils/bytes-to-string';
 
+const {
+  inject: {
+    service,
+  },
+} = Ember;
+
 export default OnePieChart.extend({
+  i18n: service(),
+
   /**
    * @implements
    */
@@ -23,14 +32,17 @@ export default OnePieChart.extend({
    * @implements
    */
   generateChartDataSeries() {
-    let _sortedData = this.get('_sortedData');
+    let {
+      _sortedData,
+      i18n,
+    } = this.getProperties('_sortedData', 'i18n');
     let chartDataSeries = this._super(...arguments);
     chartDataSeries.forEach((series, index) => {
       series.tooltipElements = [{
-        name: 'Support size',
+        name: i18n.t('components.spaceSupportChart.supportSize'),
         value: this.formatValue(_sortedData[index].value),
       }, {
-        name: 'Support share',
+        name: i18n.t('components.spaceSupportChart.supportShare'),
         value: Math.round(
           this.getSeriesPercentSize(_sortedData[index]) * 100
         ) + '%',
