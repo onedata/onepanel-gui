d8565ca95a68b496d3844183e32180a02f4f5b2a
Jakub Liput <jakub.liput@gmail.com>
2017-09-29 10:17:09 +0200

    Squashed 'src/lib/onedata-gui-common/' changes from 7918e45..fb24291
    fb24291 Merge pull request #21 in VFS/onedata-gui-common from feature/VFS-3617-onezone-mocked-backend to develop
    92751ee VFS-3617 Reverting obsolete changes in onedata-gui-common
    e508bd1 Merge commit 'e9242b27131db1439a160c19f6763701e04da43e' into feature/VFS-3617-onezone-mocked-backend
    31cfa6f Merge pull request #19 in VFS/onedata-gui-common from feature/VFS-3568-one-tree-mobile-view to develop
    443d8bc VFS-3568 mobile view for one-tree
    6611486 Merge pull request #15 in VFS/onedata-gui-common from feature/VFS-3608-table-with-support-info to develop
    8408f74 VFS-3608 pr fix
    852de4f Merge pull request #17 in VFS/onedata-gui-common from hotfix/mobile-menu-item-onclick-fix to develop
    91a230f Merge pull request #18 in VFS/onedata-gui-common from feature/VFS-3604-three-state-toggle-in-one-dynamic-tree to develop
    c919e05 Merge pull request #16 in VFS/onedata-gui-common from feature/VFS-3603-make-toggle-three-state to develop
    5a84e2c HOTFIX one-collapsible-list item header fix
    0bfe1b2 VFS-3604 styles fixes
    62c2b72 VFS-3673 Safe method exectution in space-item-sync-stats
    f51c1a6 Merge commit '95b55059c3fea3dde5ad216fe141ccdd7dfeeae7' into bugfix/VFS-3673-spaces-view-crash-10
    4c7908b VFS-3604 styles for toggle groups in one dynamic tree
    5c9ceb9 VFS-3673 Fixing infinite render loop when there arr 10 spaces on list
    55df9b7 VFS-3604 three state toggle in one-dynamic-tree
    5793171 VFS-3603 js-beautify code
    7d66d85 Merge commit '53c30f211027fabfd612fbad0badf2e839849d65' into feature/VFS-3603-make-toggle-three-state
    004db4a VFS-3603 three-state toggle
    fc88c8e VFS-3608 bugfixes
    ee67a91 Merge commit '2a266c42534b2925cce53132791c720fd2469e05' into feature/VFS-3608-table-with-support-info
    dfb85e4 VFS-3607 support-size-info component, support info components in  starages and spaces
    c5fd6e2 VFS-3608 support-size-table component
    dde848c VFS-3608 empty support-size-table component
    77346a1 HOTFIX removing development login row from common login view
    e0dc3b5 HOTFIX Fixing user record fetching in development mode
    
    git-subtree-dir: src/lib/onedata-gui-common
    git-subtree-split: fb24291d2f77cd1406210fe0eb3d6aa3cb844138

--- tests/dummy/app/components-list.js
+++ tests/dummy/app/components-list.js
@@ -4,9 +4,11 @@ export default [
   'one-dynamic-tree',
   'one-pie-chart',
   'one-tree',
+  'one-way-toggle',
   'provider-place',
   'providers-list',
   'resource-load-error',
   'space-providers-support-chart',
   'status-toolbar',
+  'support-size-info',
 ];
