252081528b038c9ea7b2eacffa7c0c4b8bcec071
Jakub Liput <jakub.liput@gmail.com>
2017-10-06 13:36:16 +0200

    VFS-3685 Major refactoring of space auto cleaning data updaters

--- src/app/components/space-auto-cleaning.js
+++ src/app/components/space-auto-cleaning.js
@@ -3,28 +3,22 @@
  * cleaning reports table.
  *
  * @module components/space-auto-cleaning
- * @author Michal Borzecki
+ * @author Michal Borzecki, Jakub Liput
  * @copyright (C) 2017 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Component from '@ember/component';
 import EmberObject from '@ember/object';
-import { computed, get, set, observer } from '@ember/object';
+import { computed, get, observer } from '@ember/object';
 import { inject } from '@ember/service';
-import { run } from '@ember/runloop';
 import { Promise } from 'rsvp';
-import moment from 'moment';
-import _ from 'lodash';
+
+import SpaceAutoCleaningUpdater from 'onepanel-gui/utils/space-auto-cleaning-updater';
 
 const BLANK_AUTO_CLEANING = {
   enabled: false,
 };
-
-const DEFAULT_UPDATE_INTERVAL = 60 * 1000;
-
-import Looper from 'onedata-gui-common/utils/looper';
-import safeMethodExecution from 'onedata-gui-common/utils/safe-method-execution';
 
 export default Component.extend({
   spaceManager: inject(),
@@ -52,6 +46,11 @@ export default Component.extend({
   autoCleaning: undefined,
 
   /**
+   * @type {SpaceAutoCleaningUpdater}
+   */
+  spaceAutoCleaningUpdater: undefined,
+
+  /**
    * Action called on autocleaning settings change.
    * @virtual
    * @type {Function}
@@ -59,23 +58,9 @@ export default Component.extend({
   updateAutoCleaning: () => {},
 
   /**
-   * Cleaning status
-   * Updated by polling
-   * @type {Onepanel.AutoCleaningStatus}
-   */
-  status: undefined,
-
-  /**
-   * Collectoion of cleaning reports
-   * Updated by polling
-   * @type {Array<Onepanel.SpaceAutoCleaningReport>}
-   */
-  reports: undefined,
-
-  /**
    * @type {Ember.ComputedProperty<boolean>}
    */
-  _isEnabled: computed.readOnly('autoCleaning.enabled'),
+  isCleanEnabled: computed.readOnly('autoCleaning.enabled'),
 
   /**
    * Data for bar chart.
@@ -117,240 +102,61 @@ export default Component.extend({
     }
   ),
 
+  /**
+   * Cleaning status
+   * Updated by polling
+   * @type {Onepanel.AutoCleaningStatus}
+   */
+  status: computed.reads('spaceAutoCleaningUpdater.status'),
+
+  /**
+   * Collectoion of cleaning reports
+   * Updated by polling
+   * @type {Array<Onepanel.SpaceAutoCleaningReport>}
+   */
+  reports: computed.reads('spaceAutoCleaningUpdater.reports'),
+
   init() {
     this._super(...arguments);
+    const {
+      autoCleaning,
+      spaceManager,
+      spaceId,
+    } = this.getProperties('autoCleaning', 'spaceManager', 'spaceId');
+
     // if the component is initialized with blank autoCleaning,
     // we should provide an empty valid autoCleaning
-    if (this.get('autoCleaning') == null) {
+    if (autoCleaning == null) {
       this.set('autoCleaning', BLANK_AUTO_CLEANING);
     }
 
-    this.setProperties({
-      _statusIsUpdating: true,
-      _reportsIsUpdating: true,
-    });
-
-    this.createWatchers();
-    this.reconfigureWatchers();
-
-    // get properties to enable observers
-    this.getProperties('_statusInterval', '_reportsInterval');
-  },
-
-  /// -- FIXME: decide these data functions should be here
-
-  /**
-   * Initialized with `createWatchers`
-   * @type {Looper}
-   */
-  _statusWatcher: undefined,
-
-  /**
-   * Initialized with `createWatchers`
-   * @type {Looper}
-   */
-  _reportsWatcher: undefined,
-
-  /**
-   * Interval [ms] used (as a one way computed property) by `_statusWatcher`
-   * @type {number}
-   */
-  _statusInterval: computed('_isEnabled', function () {
-    return this.get('_isEnabled') ? DEFAULT_UPDATE_INTERVAL : undefined;
-  }),
-
-  /**
-   * Interval [ms] used (as a one way computed property) by `_reportsWatcher`
-   * @type {number}
-   */
-  _reportsInterval: computed('_isEnabled', function () {
-    // TODO: if current auto cleaning completes, produce new value or force update
-    return this.get('_isEnabled') ? DEFAULT_UPDATE_INTERVAL : undefined;
-  }),
-
-  /**
-   * It true, currently fetching reports from backend
-   * Set by some interval watcher
-   * @type {boolean}
-   */
-  _statusIsUpdating: undefined,
-
-  /**
-   * It true, currently fetching reports from backend
-   * Set by some interval watcher
-   * @type {boolean}
-   */
-  _reportsIsUpdating: undefined,
-
-  /**
-   * True if loading status for the first time after init
-   * @type {Ember.ComputedProperty<boolean>}
-   */
-  _statusInitializing: computed(
-    'status',
-    '_statusIsUpdating',
-    function () {
-      return this.get('_statusIsUpdating') && this.get('status') == null;
-    }
-  ).readOnly(),
-
-  /**
-   * True if loading reports for the first time after init
-   * @type {Ember.ComputedProperty<boolean>}
-   */
-  _reportsInitializing: computed(
-    'reports',
-    '_reportsIsUpdating',
-    function () {
-      return this.get('_reportsIsUpdating') && this.get('reports') == null;
-    }
-  ).readOnly(),
-
-  /**
-   * Error object from fetching status rejection
-   * @type {any} typically a request error object
-   */
-  _statusError: null,
-
-  /**
-   * Error object from fetching reports rejection
-   * @type {any} typically a request error object
-   */
-  _reportsError: null,
-
-  /**
-   * @type {Ember.ComputedProperty<Moment>}
-   */
-  _reportsFetchMoment: computed('_lastReportMoment', function () {
-    const _lastReportMoment = this.get('_lastReportMoment');
-    return _lastReportMoment || defaultReportsFetchMoment();
-  }).readOnly(),
-
-  // TODO: there should be no watcher for reports at all - it should be updated:
-  // - after enabling
-  // - on change status.isWorking true -> false
-
-  /**
-   * Create watchers that will have intervals aliased from this component properties 
-   */
-  createWatchers() {
-    // TODO: refactor: code redundancy
-    const _statusWatcher = Looper.create({
-      immediate: true,
-    });
-    _statusWatcher
-      .on('tick', () =>
-        safeMethodExecution(this, 'updateStatus')
-      );
-
-    const _reportsWatcher = Looper.create({
-      immediate: true,
-    });
-    _reportsWatcher
-      .on('tick', () =>
-        safeMethodExecution(this, 'updateReports')
-      );
-
-    this.setProperties({
-      _reportsWatcher,
-      _statusWatcher,
-    });
-  },
-
-  reconfigureWatchers: observer(
-    '_statusInterval',
-    '_reportsInterval',
-    function () {
-      // debouncing does not let _setWatchersIntervals to be executed multiple
-      // times, which can occur for observer
-      run.debounce(this, '_setWatchersIntervals', 1);
-    }
-  ),
-
-  _setWatchersIntervals() {
-    // this method is invoked from debounce, so it's this can be destroyed
-    if (this.isDestroyed === false) {
-      const {
-        _statusInterval,
-        _reportsInterval,
-        _statusWatcher,
-        _reportsWatcher,
-      } = this.getProperties(
-        '_statusInterval',
-        '_reportsInterval',
-        '_statusWatcher',
-        '_reportsWatcher',
-      );
-      set(_statusWatcher, 'interval', _statusInterval);
-      set(_reportsWatcher, 'interval', _reportsInterval);
-    }
-  },
-
-  /**
-   * Last event time for reports, can be eg. `stoppedAt` moment of last report,
-   * but also can be a `startedAt` of report in progress
-   * @type {Ember.ComputedProperty<Moment>|null}
-   */
-  _lastReportMoment: computed('reports.@each.{startedAt,stoppedAt}', function () {
-    const reports = this.get('reports');
-    if (reports) {
-      return _.max(
-        _.map(reports, report => {
-          const startedAt = get(report, 'startedAt');
-          if (startedAt) {
-            return moment(startedAt);
-          } else {
-            const stoppedAt = get(report, 'stoppedAt');
-            if (stoppedAt) {
-              return moment(stoppedAt);
-            } else {
-              return null;
-            }
-          }
-        })
-      );
-    }
-  }),
-
-  updateStatus() {
-    const {
+    const spaceAutoCleaningUpdater = SpaceAutoCleaningUpdater.create({
+      isEnabled: false,
       spaceManager,
       spaceId,
-    } = this.getProperties('spaceManager', 'spaceId');
-    this.set('_statusIsUpdating', true);
-    return spaceManager.getAutoCleaningStatus(spaceId)
-      .then(autoCleaningStatus => {
-        this.set('_statusError', null);
-        return this.set('status', autoCleaningStatus);
-      })
-      .catch(error => this.set('_statusError', error))
-      .finally(() => this.set('_statusIsUpdating', false));
+    });
+    this.set('spaceAutoCleaningUpdater', spaceAutoCleaningUpdater);
+    this.toggleUpdater();
   },
 
-  updateReports() {
-    const {
-      spaceManager,
-      spaceId,
-      _reportsFetchMoment,
-    } = this.getProperties('spaceManager', 'spaceId', '_reportsFetchMoment');
-    const startedAfter = _reportsFetchMoment.toISOString();
-    this.set('_reportsIsUpdating', true);
-    return spaceManager.getAutoCleaningReports(spaceId, startedAfter)
-      .then(({ reportEntries }) => {
-        this.set('_reportsError', null);
-        return this.set('reports', reportEntries);
-      })
-      .catch(error => this.set('_reportsError', error))
-      .finally(() => this.set('_reportsIsUpdating', false));
+  willDestroyElement() {
+    this._super(...arguments);
+    this.get('spaceAutoCleaningUpdater').destroy();
   },
+
+  toggleUpdater: observer('isCleanEnabled', function () {
+    const isCleanEnabled = this.get('isCleanEnabled');
+    const spaceAutoCleaningUpdater = this.get('spaceAutoCleaningUpdater');
+    spaceAutoCleaningUpdater.set('isEnabled', isCleanEnabled);
+  }),
 
   actions: {
     toggleCleaning() {
       const {
         updateAutoCleaning,
-        _isEnabled,
-      } = this.getProperties('updateAutoCleaning', '_isEnabled');
-      return updateAutoCleaning({ enabled: !_isEnabled });
+        isCleanEnabled,
+      } = this.getProperties('updateAutoCleaning', 'isCleanEnabled');
+      return updateAutoCleaning({ enabled: !isCleanEnabled });
     },
     barValuesChanged(values) {
       let updateAutoCleaning = this.get('updateAutoCleaning');
@@ -375,7 +181,3 @@ export default Component.extend({
     },
   },
 });
-
-function defaultReportsFetchMoment() {
-  return moment().subtract(1, 'week');
-}
---
+++ src/app/utils/space-auto-cleaning-updater.js
@@ -0,0 +1,302 @@
+import { default as EmberObject, computed } from '@ember/object';
+import { get, set, observer } from '@ember/object';
+import { run } from '@ember/runloop';
+import { assert } from '@ember/debug';
+import moment from 'moment';
+import _ from 'lodash';
+
+const IDLE_UPDATE_INTERVAL = 30 * 1000;
+const WORKING_UPDATE_INTERVAL = 10 * 1000;
+
+import Looper from 'onedata-gui-common/utils/looper';
+import safeMethodExecution from 'onedata-gui-common/utils/safe-method-execution';
+
+export default EmberObject.extend({
+  /**
+   * @virtual
+   * @type {Ember.Service} SpaceManager service
+   */
+  spaceManager: undefined,
+
+  /**
+   * @virtual
+   * @type {string}
+   */
+  spaceId: undefined,
+
+  /**
+   * After init, update is disabled by default
+   * @virtual
+   * @type {boolean}
+   */
+  isEnabled: false,
+
+  status: undefined,
+
+  reports: undefined,
+
+  /**
+   * Initialized with `_createCleanWatchers`
+   * @type {Looper}
+   */
+  _cleanStatusWatcher: undefined,
+
+  /**
+   * Initialized with `_createCleanWatchers`
+   * @type {Looper}
+   */
+  _cleanReportsWatcher: undefined,
+
+  /**
+   * It true, currently fetching reports from backend
+   * Set by some interval watcher
+   * @type {boolean}
+   */
+  cleanStatusIsUpdating: undefined,
+
+  /**
+   * It true, currently fetching reports from backend
+   * Set by some interval watcher
+   * @type {boolean}
+   */
+  cleanReportsIsUpdating: undefined,
+
+  /**
+   * Error object from fetching status rejection
+   * @type {any} typically a request error object
+   */
+  cleanStatusError: null,
+
+  /**
+   * Error object from fetching reports rejection
+   * @type {any} typically a request error object
+   */
+  cleanReportsError: null,
+
+  /**
+   * @type {Ember.ComputedProperty<boolean>}
+   */
+  isWorking: computed.equal('status.isWorking', true),
+
+  /**
+   * @type {number}
+   */
+  _sharedInterval: computed('isEnabled', 'isWorking', function () {
+    if (this.get('isEnabled')) {
+      if (this.get('isWorking')) {
+        return WORKING_UPDATE_INTERVAL;
+      } else {
+        return IDLE_UPDATE_INTERVAL;
+      }
+    } else {
+      return null;
+    }
+  }),
+
+  /**
+   * Interval [ms] used by `_cleanStatusWatcher`
+   * @type {number}
+   */
+  _cleanStatusInterval: computed.reads('_sharedInterval'),
+
+  /**
+   * Interval [ms] used by `_cleanReportsWatcher`
+   * @type {number}
+   */
+  _cleanReportsInterval: computed.reads('_sharedInterval'),
+
+  /**
+   * True if loading status for the first time after init
+   * @type {Ember.ComputedProperty<boolean>}
+   */
+  cleanStatusIsInitializing: computed(
+    'status',
+    'cleanStatusIsUpdating',
+    function () {
+      return this.get('cleanStatusIsUpdating') && this.get('status') == null;
+    }
+  ).readOnly(),
+
+  /**
+   * True if loading reports for the first time after init
+   * @type {Ember.ComputedProperty<boolean>}
+   */
+  cleanReportsIsInitializing: computed(
+    'reports',
+    'cleanReportsIsUpdating',
+    function () {
+      return this.get('cleanReportsIsUpdating') && this.get('reports') == null;
+    }
+  ).readOnly(),
+
+  /**
+   * @type {Ember.ComputedProperty<Moment>}
+   */
+  _cleanReportsFetchMoment: computed('_lastReportMoment', function () {
+    return this.get('_lastReportMoment') || defaultReportsFetchMoment();
+  }).readOnly(),
+
+  /**
+   * Last event time for reports, can be eg. `stoppedAt` moment of last report,
+   * but also can be a `startedAt` of report in progress
+   * @type {Ember.ComputedProperty<Moment>|null}
+   */
+  _lastReportMoment: computed('reports.@each.{startedAt,stoppedAt}', function () {
+    const reports = this.get('reports');
+    if (reports) {
+      return _.max(
+        _.map(reports, report => {
+          const startedAt = get(report, 'startedAt');
+          if (startedAt) {
+            return moment(startedAt);
+          } else {
+            const stoppedAt = get(report, 'stoppedAt');
+            if (stoppedAt) {
+              return moment(stoppedAt);
+            } else {
+              return null;
+            }
+          }
+        })
+      );
+    }
+  }),
+
+  init() {
+    this._super(...arguments);
+    const {
+      spaceManager,
+      spaceId,
+    } = this.getProperties('spaceManager', 'spaceId');
+
+    assert(typeof spaceManager !== 'object', 'spaceManager service should be injected');
+    assert(typeof spaceId !== 'string', 'spaceId should be injected');
+
+    this.setProperties({
+      cleanStatusIsUpdating: true,
+      cleanReportsIsUpdating: true,
+    });
+
+    this._createCleanWatchers();
+    this._reconfigureCleanWatchers();
+
+    // get properties to enable observers
+    this.getProperties('_cleanStatusInterval', '_cleanReportsInterval');
+  },
+
+  destroy() {
+    try {
+      this.setProperties({
+        _cleanStatusInterval: undefined,
+        _cleanReportsInterval: undefined,
+      });
+      _.each(
+        _.values(this.getProperties('_cleanStatusWatcher', '_cleanReportsWatcher')),
+        watcher => watcher.destroy()
+      );
+    } finally {
+      this._super(...arguments);
+    }
+  },
+
+  _reconfigureCleanWatchers: observer(
+    '_cleanStatusInterval',
+    '_cleanReportsInterval',
+    function () {
+      // debouncing does not let _setCleanWatchersIntervals to be executed multiple
+      // times, which can occur for observer
+      run.debounce(this, '_setCleanWatchersIntervals', 1);
+    }
+  ),
+
+  // TODO: there should be no watcher for reports at all - it should be updated:
+  // - after enabling
+  // - on change status.isWorking true -> false
+
+  /**
+   * Create watchers for fetching information space cleaning
+   */
+  _createCleanWatchers() {
+    const _cleanStatusWatcher = Looper.create({
+      immediate: true,
+    });
+    _cleanStatusWatcher
+      .on('tick', () =>
+        safeMethodExecution(this, 'fetchCleanStatus')
+      );
+
+    const _cleanReportsWatcher = Looper.create({
+      immediate: true,
+    });
+    _cleanReportsWatcher
+      .on('tick', () =>
+        safeMethodExecution(this, 'fetchCleanReports')
+      );
+
+    this.setProperties({
+      _cleanReportsWatcher,
+      _cleanStatusWatcher,
+    });
+  },
+
+  _setCleanWatchersIntervals() {
+    // this method is invoked from debounce, so it's this can be destroyed
+    if (this.isDestroyed === false) {
+      const {
+        _cleanStatusInterval,
+        _cleanReportsInterval,
+        _cleanStatusWatcher,
+        _cleanReportsWatcher,
+      } = this.getProperties(
+        '_cleanStatusInterval',
+        '_cleanReportsInterval',
+        '_cleanStatusWatcher',
+        '_cleanReportsWatcher',
+      );
+      set(_cleanStatusWatcher, 'interval', _cleanStatusInterval);
+      set(_cleanReportsWatcher, 'interval', _cleanReportsInterval);
+    }
+  },
+
+  fetchCleanStatus() {
+    const {
+      spaceManager,
+      spaceId,
+    } = this.getProperties('spaceManager', 'spaceId');
+    this.set('cleanStatusIsUpdating', true);
+    return spaceManager.getAutoCleaningStatus(spaceId)
+      .then(autoCleaningStatus => {
+        this.set('cleanStatusError', null);
+        return this.set('status', autoCleaningStatus);
+      })
+      .catch(error => this.set('cleanStatusError', error))
+      .finally(() => this.set('cleanStatusIsUpdating', false));
+  },
+
+  fetchCleanReports() {
+    const {
+      spaceManager,
+      spaceId,
+      _cleanReportsFetchMoment,
+    } = this.getProperties('spaceManager', 'spaceId', '_cleanReportsFetchMoment');
+    const startedAfter = _cleanReportsFetchMoment.toISOString();
+    this.set('cleanReportsIsUpdating', true);
+    return spaceManager.getAutoCleaningReports(spaceId, startedAfter)
+      .then(({ reportEntries }) => {
+        this.set('cleanReportsError', null);
+        return this.set('reports', reportEntries);
+      })
+      .catch(error => this.set('cleanReportsError', error))
+      .finally(() => this.set('cleanReportsIsUpdating', false));
+  },
+
+});
+
+/**
+ * If there is no information about last cleaning event,
+ * we should fetch reports that started after this Moment
+ * @returns {Moment}
+ */
+function defaultReportsFetchMoment() {
+  return moment().subtract(1, 'week');
+}
