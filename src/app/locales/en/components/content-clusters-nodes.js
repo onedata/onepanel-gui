3940be08ef4c1e0fdfc8b7400b0afea01494f654
Jakub Liput <jakub.liput@gmail.com>
2018-03-05 12:24:47 +0100

    VFS-4097 Style fixes

--- src/app/locales/en/components/content-clusters-nodes.js
+++ src/app/locales/en/components/content-clusters-nodes.js
@@ -2,12 +2,7 @@ export default {
   header: 'Nodes of "{{name}}"',
   tabServices: 'Services',
   tabIps: 'Cluster IPs',
-
   editIps: 'Edit IPs',
   cancelIpsEdit: 'Cancel editing',
-
-  // FIXME: translations copied from new-cluster-ips
   setupIpsButton: 'Confirm changes',
-  clusterHosts: 'Cluster hosts',
-  configurationInvalid: 'Configuration is invalid or incomplete',
 };
--- src/app/locales/en/components/new-cluster-ips.js
+++ src/app/locales/en/components/new-cluster-ips.js
@@ -1,7 +1,6 @@
 export default {
   header: 'IP adresses setup',
   setupIpsButton: 'Setup IP adresses',
-  clusterHosts: 'Cluster hosts',
 
   infoAutoDetect: 'Below IP adresses are auto-detected suggestions that need to ' +
     'be revised. Please make sure that your cluster nodes are reachable at ' +
