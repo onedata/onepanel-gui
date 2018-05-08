3a3a01f0f4e583d9e561e2bd6597de0bd455e084
Jakub Liput <jakub.liput@gmail.com>
2018-05-08 15:17:59 +0200

    VFS-4381 Removed signature version option from S3 storage form

--- src/app/utils/cluster-storage/s3-fields.js
+++ src/app/utils/cluster-storage/s3-fields.js
@@ -3,15 +3,6 @@ export default [
   { name: 'secretKey', type: 'password' },
   { name: 'hostname', type: 'text', defaultValue: 's3.amazonaws.com' },
   { name: 'bucketName', type: 'text' },
-  {
-    name: 'signatureVersion',
-    type: 'radio-group',
-    defaultValue: 4,
-    options: [
-      { value: 4, label: '4' },
-      { value: 2, label: '2' },
-    ],
-  },
   { name: 'blockSize', type: 'number', optional: true },
   { name: 'timeout', type: 'number', optional: true },
   { name: 'insecure', type: 'checkbox', optional: true },
