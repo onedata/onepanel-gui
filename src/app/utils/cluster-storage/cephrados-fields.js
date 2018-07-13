66920c30466eae1df10107f8c6cf519d775adaae
Jakub Liput <jakub.liput@gmail.com>
2018-07-13 14:42:52 +0200

    VFS-4677 PR fixes

--- src/app/utils/cluster-storage/cephrados-fields.js
+++ src/app/utils/cluster-storage/cephrados-fields.js
@@ -2,22 +2,16 @@ import cephFields from './ceph-fields';
 import _ from 'lodash';
 
 const separatorKey = 'poolName';
-const cephTop = [];
-const cephBottom = [];
-
-let areTopKeys = true;
-cephFields.forEach(field => {
-  (areTopKeys ? cephTop : cephBottom).push(field);
-  if (field.name === separatorKey) {
-    areTopKeys = false;
-  }
-});
+const separatorIndexNext = _.findIndex(cephFields, { name: separatorKey }) + 1;
+const cephTop = cephFields.slice(0, separatorIndexNext);
+const cephBottom = cephFields.slice(separatorIndexNext, cephFields.length);
 
 const cephRadosSpecific = [{
   name: 'blockSize',
   type: 'number',
   defaultValue: 4194304,
   example: '4194304',
+  gt: 0,
 }];
 
 export default _.concat(cephTop, cephRadosSpecific, cephBottom);
