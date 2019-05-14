f8ae7f689f7007f3d2a3e4de617e0f5aa952281e
Jakub Liput <jakub.liput@gmail.com>
2019-05-14 09:59:44 +0200

    VFS-5342 Layout fixes and improvements

--- src/app/components/app-layout.js
+++ src/app/components/app-layout.js
@@ -1,44 +1,14 @@
 /**
- * Main layout and components of application in authenticated mode
+ * Main layout and components of application in authenticated mode.
+ * Onepanel specific - adds WithBottomBar mixin.
  * 
  * @module components/app-layout
  * @author Jakub Liput
- * @copyright (C) 2017-2019 ACK CYFRONET AGH
+ * @copyright (C) 2019 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import AppLayout from 'onedata-gui-common/components/app-layout';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
-import { reads } from '@ember/object/computed';
+import WithBottomBar from 'onepanel-gui/mixins/components/with-bottom-bar';
 
-export default AppLayout.extend({
-  media: service(),
-  deploymentManager: service(),
-  onepanelServer: service(),
-
-  isEmergencyOnepanel: reads('onepanelServer.isEmergency'),
-
-  /**
-   * Deployment manager's installationDetails should be available always
-   * in `onedata` routes because it is blocking `onedata` model.
-   */
-  isDeploying: computed(
-    'deploymentManager.installationDetails.isInitialized',
-    function isDeploying() {
-      return this.get('deploymentManager.installationDetails.isInitialized') ===
-        false;
-    },
-  ),
-
-  withBottomBar: computed(
-    'isEmergencyOnepanel',
-    'isDeploying',
-    'media.{isDesktop,isTablet}',
-    function withBottomBar() {
-      return this.get('isDeploying') && this.get('isEmergencyOnepanel') && (
-        this.get('media.isDesktop') || this.get('media.isTablet')
-      );
-    }
-  ),
-});
+export default AppLayout.extend(WithBottomBar);
