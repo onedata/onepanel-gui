81a2fcc0e92fc8950e4f50893a82c39df8c19f51
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-06-09 20:09:24 +0200

    VFS-5496 updated translations

--- src/app/locales/en/components/sidebar-clusters.js
+++ src/app/locales/en/components/sidebar-clusters.js
@@ -2,6 +2,6 @@ export default {
   secondLevelItems: {
     dnsWarning: 'Some problems with your DNS configuration have been detected',
     webCertWarning: 'Some problems with your web certificate have been detected',
-    noConnectedUsersWarning: 'This cluster has no connected Onezone users',
+    noConnectedUsersWarning: 'This cluster does not have any linked Onezone user',
   },
 };
