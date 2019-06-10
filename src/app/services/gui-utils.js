4d5197e8d36b5cfc97744102911b23c6305ac4b7
Jakub Liput <jakub.liput@gmail.com>
2019-06-10 17:19:40 +0200

    VFS-5476 Fixing link to account settings

--- src/app/services/gui-utils.js
+++ src/app/services/gui-utils.js
@@ -87,7 +87,7 @@ export default GuiUtils.extend(
       'onezoneGui.onezoneGuiUrl',
       function manageAccountExternalLink() {
         const onezoneGuiUrl = this.get('onezoneGui.onezoneGuiUrl');
-        return `${onezoneGuiUrl}/onedata/users`;
+        return `${onezoneGuiUrl}onedata/users`;
       }
     ),
 
