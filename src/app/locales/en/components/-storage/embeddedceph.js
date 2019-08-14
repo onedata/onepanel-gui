a1caa6773a5b098758860620a00d5bd34a1b49c3
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-08-14 12:34:20 +0200

    VFS-5020 added custom messages to copies number fields

--- src/app/locales/en/components/-storage/embeddedceph.js
+++ src/app/locales/en/components/-storage/embeddedceph.js
@@ -1,6 +1,12 @@
 export default {
-  copiesNumber: { name: 'Number of copies' },
-  minCopiesNumber: { name: 'Min. number of copies' },
+  copiesNumber: {
+    name: 'Number of copies',
+    lteMessage: 'This field must be less than or equal to the number of Ceph OSDs',
+  },
+  minCopiesNumber: {
+    name: 'Min. number of copies',
+    lteMessage: 'This field must be less than or equal to the number of copies',
+  },
   timeout: { name: 'Timeout [ms]' },
   insecure: { name: 'Insecure' },
   readonly: { name: 'Read only' },
--- src/app/utils/cluster-storage/embeddedceph-fields.js
+++ src/app/utils/cluster-storage/embeddedceph-fields.js
@@ -1,7 +1,38 @@
 export default [
-  { name: 'copiesNumber', type: 'number', defaultValue: 1, gte: 1, lte: { property: 'meta.osdsNumber' } },
-  { name: 'minCopiesNumber', type: 'number', defaultValue: 1, gte: 1, lte: { property: 'embeddedceph.copiesNumber' } },
-  { name: 'timeout', type: 'number', optional: true },
-  { name: 'insecure', type: 'checkbox', optional: true, defaultValue: true },
-  { name: 'readonly', type: 'checkbox', optional: true },
+  {
+    name: 'copiesNumber',
+    type: 'number',
+    defaultValue: 1,
+    gte: 1,
+    lte: {
+      property: 'meta.osdsNumber',
+      message: 'components.clusterStorageAddForm.embeddedceph.copiesNumber.lteMessage',
+    },
+  },
+  {
+    name: 'minCopiesNumber',
+    type: 'number',
+    defaultValue: 1,
+    gte: 1,
+    lte: {
+      property: 'embeddedceph.copiesNumber',
+      message: 'components.clusterStorageAddForm.embeddedceph.minCopiesNumber.lteMessage',
+    },
+  },
+  {
+    name: 'timeout',
+    type: 'number',
+    optional: true,
+  },
+  {
+    name: 'insecure',
+    type: 'checkbox',
+    optional: true,
+    defaultValue: true,
+  },
+  {
+    name: 'readonly',
+    type: 'checkbox',
+    optional: true,
+  },
 ];
