15de02befdcd6e9098f4ea4473e66d430bb7e043
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-08-23 17:05:57 +0200

    VFS-1891 added navigation using queryParams

--- src/app/components/content-clusters-gui-settings.js
+++ src/app/components/content-clusters-gui-settings.js
@@ -1,20 +1,67 @@
 import Component from '@ember/component';
 import I18n from 'onedata-gui-common/mixins/components/i18n';
-// import DOMPurify from 'npm:dompurify';
-// import { computed } from '@ember/object';
-// import { htmlSafe } from '@ember/string';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { next } from '@ember/runloop';
+import safeExec from 'onedata-gui-common/utils/safe-method-execution';
+
+const allowedTabs = ['signInNotification', 'privacyPolicy'];
 
 export default Component.extend(I18n, {
   classNames: ['content-clusters-gui-settings'],
+
+  i18n: service(),
+  router: service(),
 
   /**
    * @override
    */
   i18nPrefix: 'components.contentClustersGuiSettings',
 
-  // value: '<a onclick="javascript:alert(\'asdf\')">ddd</a>',
+  /**
+   * @type {string}
+   */
+  activeTab: 'status',
 
-  // safeValue: computed('value', function () {
-  //   return htmlSafe(DOMPurify.sanitize(this.get('value')));
-  // }),
+  /**
+   * Tab id is in format `elementId-tab`, where tab value corresponds to
+   * activeTab property possible values.
+   * @type {Ember.ComputedProperty<string>}
+   */
+  activeTabId: computed('elementId', 'activeTab', function activeTabId() {
+    const {
+      elementId,
+      activeTab,
+    } = this.getProperties('elementId', 'activeTab');
+    return `${elementId}-${activeTab}`;
+  }),
+
+  init() {
+    this._super(...arguments);
+
+    if (!allowedTabs.includes(this.get('activeTab'))) {
+      next(() => safeExec(this, () => this.changeTab(allowedTabs[0])));
+    }
+  },
+
+  /**
+   * @param {string} tabName
+   * @returns {undefined}
+   */
+  changeTab(tabName) {
+    if (allowedTabs.includes(tabName)) {
+      this.get('router').transitionTo({
+        queryParams: {
+          options: `tab.${tabName}`,
+        },
+      });
+    }
+  },
+
+  actions: {
+    tabChange(tabId) {
+      const tab = tabId.split('-')[1];
+      this.changeTab(tab);
+    },
+  },
 });
