7bcca8eb3da17f0549faac50bfa9679d9388ba1a
Jakub Liput <jakub.liput@gmail.com>
2020-01-31 12:45:07 +0100

    VFS-5988 PR fixes

--- src/app/components/sidebar-shares.js
+++ src/app/components/sidebar-shares.js
@@ -1,3 +1,12 @@
+/**
+ * Opening this sidebar will cause to redirect into Onezone shares view
+ * 
+ * @module components/sidebar-shares
+ * @author Jakub Liput
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import contentClustersOnezoneRedirect from 'onepanel-gui/components/content-clusters-onezone-redirect';
 
 export default contentClustersOnezoneRedirect.extend({
--- src/app/components/sidebar-spaces.js
+++ src/app/components/sidebar-spaces.js
@@ -1,3 +1,12 @@
+/**
+ * Opening this sidebar will cause to redirect into Onezone spaces (data) view
+ * 
+ * @module components/sidebar-spaces
+ * @author Jakub Liput
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import contentClustersOnezoneRedirect from 'onepanel-gui/components/content-clusters-onezone-redirect';
 
 export default contentClustersOnezoneRedirect.extend({
