75474c6e9c3f03e6f8d5ef1db597299f3f04cde7
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-06-25 16:55:20 +0200

    VFS-6352 fixed qos params header, better luma fields labels

--- src/app/locales/en/components/-storage/luma.js
+++ src/app/locales/en/components/-storage/luma.js
@@ -1,10 +1,10 @@
 export default {
   lumaFeedUrl: {
-    name: 'LUMA feed URL',
+    name: 'External LUMA feed URL',
     tip: 'URL of external feed for LUMA DB, must be reachable from all Oneprovider nodes.',
   },
   lumaFeedApiKey: {
-    name: 'LUMA feed API key',
+    name: 'External LUMA feed API key',
     tip: 'Optional, arbitrary alphanumeric string that serves as authorization in requests to external feed for LUMA DB. If not specified, no authorization headers will be sent.',
   },
 };
