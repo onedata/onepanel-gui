c4af73bfda266211e0d7d4f7cbcdb28fb4f11c7b
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-09-15 18:23:05 +0200

    VFS-6330 added new errors handling and documentation for start/stop scan button

--- src/app/components/space-storage-import/start-stop-scan-button.js
+++ src/app/components/space-storage-import/start-stop-scan-button.js
@@ -1,3 +1,14 @@
+/**
+ * Renders button for starting and stopping storage import scan. The button changes
+ * between start and stop mode (including disabled state in some cases) depending on import
+ * status.
+ *
+ * @module components/space-storage-import/start-stop-scan-button
+ * @author Michał Borzęcki
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import Component from '@ember/component';
 import notImplementedReject from 'onedata-gui-common/utils/not-implemented-reject';
 import { reads } from '@ember/object/computed';
