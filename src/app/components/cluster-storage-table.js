21500721d21fb90efb2a450a50243bbb8ab46c61
Jakub Liput <jakub.liput@gmail.com>
2023-06-05 07:32:35 +0200

    VFS-10875 Fixed ESLint

--- src/app/components/cluster-storage-table.js
+++ src/app/components/cluster-storage-table.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { array } from 'ember-awesome-macros';
 
 /**
  * Creates component for displaying cluster storages.
