8cf34b6ff6b1baf5e44fa3fe9b7d15f5fc03635c
Jakub Liput <jakub.liput@gmail.com>
2024-02-23 08:47:06 +0100

    VFS-11710 Show information message when trying to open any route in iframed panel

--- src/app/router.js
+++ src/app/router.js
@@ -2,14 +2,16 @@
  * Using standard routes and configuration from onedata-gui-common addon
  *
  * @author Jakub Liput
- * @copyright (C) 2017 ACK CYFRONET AGH
+ * @copyright (C) 2017-2024 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import OnedataRouter from 'onedata-gui-common/utils/onedata-router';
 import onedataRouterSetup from 'onedata-gui-common/utils/onedata-router-setup';
-
+import globals from 'onedata-gui-common/utils/globals';
 import config from './config/environment';
+
+const isInIframe = globals.window.parent !== globals.window;
 
 const Router = OnedataRouter.extend({
   location: config.locationType,
@@ -18,7 +20,11 @@ const Router = OnedataRouter.extend({
 
 // eslint-disable-next-line array-callback-return
 Router.map(function () {
-  onedataRouterSetup(Router, this);
+  // There are no public routes (available in the iframe embedded in foreign site)
+  // in Onepanel.
+  if (!isInIframe) {
+    onedataRouterSetup(Router, this);
+  }
 });
 
 export default Router;
