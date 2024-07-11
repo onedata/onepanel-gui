a721b80b7c7a52311c0510d0e1e98be040d59367
plgagneska <agawarchol15@gmail.com>
2024-07-11 16:40:27 +0200

    VFS-12052 Change texts in save storage modification ack modal

--- src/app/components/modals/save-storage-modification-ack-modal.js
+++ src/app/components/modals/save-storage-modification-ack-modal.js
@@ -59,10 +59,11 @@ export default Component.extend(I18n, {
   /**
    * @type {ComputedProperty<Array<SafeString>>}
    */
-  warningTexts: computed('warnings', function warningTexts() {
-    return [...this.warnings].sort().map((warningType) =>
-      this.t(`warnings.${warningType}`)
-    );
+  warningText: computed('warnings', function warningText() {
+    if (this.warnings.includes('qos')) {
+      return this.t('warnings.qos');
+    }
+    return '';
   }),
 
   actions: {
--- src/app/locales/en/components/modals/save-storage-modification-ack-modal.js
+++ src/app/locales/en/components/modals/save-storage-modification-ack-modal.js
@@ -1,11 +1,10 @@
 export default {
   header: 'Modify storage backend',
-  warningsIntro: 'Your modification can cause below issues and/or will need additional manual steps:',
+  warningsIntro: 'Before proceeding, double-check the updated configuration. ',
   warnings: {
-    restart: 'The changes in storage configuration will not take effect until Oneprovider and attached Oneclient instances are restarted. This behaviour will be improved in future releases of Onedata.',
-    qos: 'Modification of QoS parameters <strong>will not trigger recalculation</strong> of the existing QoS requirements assigned to user files in the supported spaces. Only newly created requirements will use the new parameters. This behaviour will be improved in future releases of Onedata.',
+    qos: '<strong>Note:</strong> modification of QoS parameters <strong>will not trigger recalculation</strong> of the existing QoS requirements assigned to user files in the supported spaces. Only newly created requirements will use the new parameters. This behaviour will be improved in future releases of Onedata.',
   },
-  ackMessage: 'I understand that incorrect storage configuration can cause data loss, corruption, or discrepancies between file metadata and content in all supported spaces. ',
+  ackMessage: 'I understand that an incorrect storage backend configuration can cause data loss, corruption, or discrepancies between file metadata and content in all supported spaces.',
   buttons: {
     cancel: 'Cancel',
     proceed: 'Proceed',
