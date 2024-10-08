744c5660b686273711ffc1911594b8e483173574
Jakub Liput <jakub.liput@gmail.com>
2024-10-08 11:19:05 +0200

    VFS-11890 Moved sync observers check to after each test

--- src/tests/test-helper.js
+++ src/tests/test-helper.js
@@ -1,27 +1,3 @@
-import { start, setResolver } from 'ember-mocha';
-import { mocha, afterEach } from 'mocha';
-import { setApplication } from '@ember/test-helpers';
-import resolver from './helpers/resolver';
-import Application from '../app';
-import config from '../config/environment';
-import { unsuppressRejections } from './helpers/suppress-rejections';
-import checkSyncObservers from './helpers/check-sync-observers';
-import handleHidepassed from './handle-hidepassed';
-import sinon from 'sinon';
-import globals from 'onedata-gui-common/utils/globals';
+import commonTestHelper from './helpers/common-test-helper';
 
-mocha.setup({
-  timeout: 5000,
-});
-setResolver(resolver);
-setApplication(Application.create(config.APP));
-
-afterEach(unsuppressRejections);
-afterEach(() => sinon.restore());
-afterEach(() => globals.unmock());
-afterEach(() => {
-  checkSyncObservers();
-});
-
-handleHidepassed(afterEach);
-start();
+commonTestHelper();
