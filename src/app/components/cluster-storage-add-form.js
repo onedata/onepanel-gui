ac47a201d6951322211fc095430c6792b441ab5a
plgagneska <agawarchol15@gmail.com>
2025-09-04 11:57:19 +0200

    VFS-12598 Changed create fields in storage form

--- src/app/components/cluster-storage-add-form.js
+++ src/app/components/cluster-storage-add-form.js
@@ -190,35 +190,105 @@ export default Component.extend(I18n, {
       });
   }),
 
-  fieldsArray: computed('storage', function fieldsArray() {
-    const formContext = EmberObject.extend({
+  context: computed(function () {
+    return EmberObject.extend({
       editorMode: reads('component.mode'),
       loadedStorage: reads('component.storage'),
     }).create({ component: this });
-
-    const fieldsList = [BasicGroup, LumaGroup];
-    const fieldGroupClassMapping = {
-      posix: PosixGroup,
-      nfs: NfsGroup,
-      s3: S3Group,
-      swift: SwiftGroup,
-      glusterfs: GlusterfsGroup,
-      cephrados: CephRadosGroup,
-      http: HttpGroup,
-      webdav: WebdavGroup,
-      xrootd: XrootdGroup,
-      nulldevice: NullDeviceGroup,
-    };
-    if (this.storage) {
-      fieldsList.push(fieldGroupClassMapping[this.storage.type]);
-    } else {
-      fieldsList.push(...Object.values(fieldGroupClassMapping));
-    }
-
-    return fieldsList.map(
-      (FieldClass) => FieldClass.create({ context: formContext })
-    );
   }),
+
+  basicGroupFields: computed(function basicGroupFields() {
+    return BasicGroup.create({ context: this.context });
+  }),
+
+  lumaGroupFields: computed(function lumaGroupFields() {
+    return LumaGroup.create({ context: this.context });
+  }),
+
+  posixGroupFields: computed(function posixGroupFields() {
+    return PosixGroup.create({ context: this.context });
+  }),
+
+  nfsGroupFields: computed(function nfsGroupFields() {
+    return NfsGroup.create({ context: this.context });
+  }),
+
+  s3GroupFields: computed(function s3GroupFields() {
+    return S3Group.create({ context: this.context });
+  }),
+
+  swiftGroupFields: computed(function swiftGroupFields() {
+    return SwiftGroup.create({ context: this.context });
+  }),
+
+  glusterfsGroupFields: computed(function glusterfsGroupFields() {
+    return GlusterfsGroup.create({ context: this.context });
+  }),
+
+  cephRadosGroupFields: computed(function cephRadosGroupFields() {
+    return CephRadosGroup.create({ context: this.context });
+  }),
+
+  httpGroupFields: computed(function httpGroupFields() {
+    return HttpGroup.create({ context: this.context });
+  }),
+
+  webdavGroupFields: computed(function webdavGroupFields() {
+    return WebdavGroup.create({ context: this.context });
+  }),
+
+  xrootdGroupFields: computed(function xrootdGroupFields() {
+    return XrootdGroup.create({ context: this.context });
+  }),
+
+  nullDeviceGroupFields: computed(function nullDeviceGroupFields() {
+    return NullDeviceGroup.create({ context: this.context });
+  }),
+
+  fieldsArray: computed(
+    'storage',
+    'basicGroupFields.value.type',
+    function fieldsArray() {
+      const fieldsList = [this.basicGroupFields, this.lumaGroupFields];
+
+      const type = this.storage?.type || this.basicGroupFields?.value?.type || 'cephrados';
+      switch (type) {
+        case 'posix':
+          fieldsList.push(this.posixGroupFields);
+          break;
+        case 'nfs':
+          fieldsList.push(this.nfsGroupFields);
+          break;
+        case 's3':
+          fieldsList.push(this.s3GroupFields);
+          break;
+        case 'swift':
+          fieldsList.push(this.swiftGroupFields);
+          break;
+        case 'glusterfs':
+          fieldsList.push(this.glusterfsGroupFields);
+          break;
+        case 'cephrados':
+          fieldsList.push(this.cephRadosGroupFields);
+          break;
+        case 'http':
+          fieldsList.push(this.httpGroupFields);
+          break;
+        case 'webdav':
+          fieldsList.push(this.webdavGroupFields);
+          break;
+        case 'xrootd':
+          fieldsList.push(this.xrootdGroupFields);
+          break;
+        case 'nulldevice':
+          fieldsList.push(this.nullDeviceGroupFields);
+          break;
+        default:
+          break;
+      }
+      return fieldsList;
+    }
+  ),
 
   /**
    * @type {ComputedProperty<Utils.FormComponent.FormFieldsGroup>}
