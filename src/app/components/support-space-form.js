e8f036b7046bfbc3280a74313527a923540b073d
plgagneska <agawarchol15@gmail.com>
2026-05-14 14:56:05 +0200

    VFS-13521 Resolved PR tasks

--- src/app/components/support-space-form.js
+++ src/app/components/support-space-form.js
@@ -24,6 +24,7 @@ import { and, or, not, isEmpty } from 'ember-awesome-macros';
 import trimToken from 'onedata-gui-common/utils/trim-token';
 import PromiseObject from 'onedata-gui-common/utils/ember/promise-object';
 import notImplementedReject from 'onedata-gui-common/utils/not-implemented-reject';
+import { serializeAspectOptions } from 'onedata-gui-common/services/navigation-state';
 
 const units = _.find(formFields, { name: 'sizeUnit' }).options;
 
@@ -42,7 +43,6 @@ export default OneFormSimple.extend(I18n, buildValidations(valdiationsProto), {
   globalNotify: service(),
   alertService: service('alert'),
   router: service(),
-  navigationState: service(),
 
   /**
    * @override
@@ -268,14 +268,16 @@ export default OneFormSimple.extend(I18n, buildValidations(valdiationsProto), {
           globalNotify.backendError('space supporting', error);
           throw error;
         })
-        .then(result => {
-          this.get('navigationState').changeRouteAspectOptions({
-            space: result?.data?.id,
-            tab: 'overview',
-            storageId: null,
-            isFormOpened: null,
-          });
-        });
+        .then(result =>
+          this.router.transitionTo('onedata.sidebar.content.aspect', 'spaces', {
+            queryParams: {
+              options: serializeAspectOptions({
+                space: result?.data?.id,
+                tab: 'overview',
+              }),
+            },
+          })
+        );
     },
     storageChanged(storageItem) {
       this.set('selectedStorageItem', storageItem);
